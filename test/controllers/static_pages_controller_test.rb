7a7a677fa2d04ab713a3fa398ba3123aa27627f6
DennisDaddy <softteez95@gmail.com>
2016-12-22 12:44:50 +0300

    add updates

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ruby on Rails"
+  end
+
+
+  test "should get home" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Ruby on Rails"
+    
+  end
+
+  test "should get help" do
+    get help_url
+    assert_response :success
+    assert_select "title", "Help | Ruby on Rails"
+  end
+
+  test "Should get about" do
+    get about_url
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails"
+    end
+
+end
