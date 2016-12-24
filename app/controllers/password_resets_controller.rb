72fd2c1b0a4f29508361f30d94a55ff887bb4061
DennisDaddy <softteez95@gmail.com>
2016-12-24 17:15:56 +0300

    add update action for password reset.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -23,16 +23,44 @@ end
   def edit
   end
 
+  def update
+	if password_blank?
+	flash.now[:danger] = "Password can't be blank"
+	render 'edit'
+	elsif @user.update_attributes(user_params)
+	log_in @user
+	flash[:success] = "Password has been reset."
+	redirect_to @user
+	else
+	render 'edit'
+  end 
+end
+
   private
 
+		def user_params
+	    params.require(:user).permit(:password, :password_confirmation)
+	end
+	# Returns true if password is blank.
+	def password_blank?
+	   params[:user][:password].blank?
+	end
+	# Before filters
 	def get_user
-	@user = User.find_by(email: params[:email])
+	  @user = User.find_by(email: params[:email])
 	end
 	# Confirms a valid user.
 	def valid_user
-	unless (@user && @user.activated? &&
-	@user.authenticated?(:reset, params[:id]))
-	redirect_to root_url
+		unless (@user && @user.activated? &&
+		@user.authenticated?(:reset, params[:id]))
+		redirect_to root_url
 	end
-  end
+	end
+	# Checks expiration of reset token.
+	def check_expiration
+		if @user.password_reset_expired?
+		flash[:danger] = "Password reset has expired."
+		redirect_to new_password_reset_url
+	end
+   end
 end
